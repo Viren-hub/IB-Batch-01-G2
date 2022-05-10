6b4ebba1c6b297eec9702287ebfc96f11a435f63
Rajesh Sonawane <annuraj261998@gmail.com>
2022-05-10 08:04:26 +0530

    cards added above the table for total number of count

--- covid.js
+++ covid.js
@@ -1,5 +1,5 @@
-var loader=`<div id="loading"  style="margin-left:500px;"></div>`
-document.getElementById("data").innerHTML=loader;
+var loader = `<div id="loading"  style="margin-left:500px;"></div>`
+document.getElementById("data").innerHTML = loader;
 
 
 
@@ -18,7 +18,7 @@ function closeFunction() {
 function getdata() {
     // alert("hi")
     var name = document.getElementById('input').value;
-    document.getElementById("data").innerHTML=loader;
+    document.getElementById("data").innerHTML = loader;
     fetch(`https://api.covid19api.com/dayone/country/${name}`).then(
 
         res => {
@@ -49,35 +49,34 @@ function getdata() {
 }
 fetch("https://api.covid19api.com/dayone/country/india").then(
 
-        res => {
-            res.json().then(
-                data => {
-                    if (data.length > 0) {
-                        data = data.reverse();
-                        var temp = "";
-                        data.forEach((itemData) => {
-                            temp += "<tr>";
-                            temp += "<td >" + itemData.ID + "</td>";
-                            temp += "<td>" + itemData.Country + "</td>";
-                            temp += "<td>" + itemData.CountryCode + "</td>";
-                            temp += "<td>" + itemData.Lat + "</td>";
-                            temp += "<td>" + itemData.Lon + "</td>";
-                            temp += "<td>" + itemData.Confirmed + "</td>";
-                            temp += "<td>" + itemData.Deaths + "</td>";
-                            temp += "<td>" + itemData.Recovered + "</td>";
-                            temp += "<td>" + itemData.Active + "</td>";
-                            temp += "<td>" + new Date(itemData.Date).toLocaleDateString() + "</td></tr>";
-                        });
-                        document.getElementById('data').innerHTML = temp;
-                    }
+    res => {
+        res.json().then(
+            data => {
+                if (data.length > 0) {
+                    data = data.reverse();
+                    var temp = "";
+                    data.forEach((element) => {
+                        temp += "<tr>";
+                        temp += `<td class="idData">${element.ID}</td>`
+                        temp += `<td class="idData">${element.Country}</td>`
+                        temp += `<td class="idData">${element.CountryCode}</td>`
+                        temp += `<td class="idData">${element.Lat}</td>`
+                        temp += `<td class="idData">${element.Lon}</td>`
+                        temp += `<td class="idData">${element.Confirmed}</td>`
+                        temp += `<td class="idData">${element.Deaths}</td>`
+                        temp += `<td class="idData">${element.Recovered}</td>`
+                        temp += `<td class="idData">${element.Active}</td>`
+                        temp += `<td class="idData">${new Date(values.Date).toLocaleDateString()}</td>`
+                    });
+                    document.getElementById('data').innerHTML = temp;
                 }
-            )
-        }
-    )
-// function ready() {
-//     document.getElementById("body").show();
-//     document.getElementById("msg").hide();
-// }
+            }
+        )
+    }
+)
+
+
+//============ Searching Bar ================= 
 
 function myFunction() {
     // Declare variables
@@ -103,30 +102,18 @@ function myFunction() {
 
 
 
-// function sortTable() {
-//     var filterTable, rows, sorted, i, x, y, sortFlag;
-//     filterTable = document.getElementById('act');
-//     sorted = true;
-//     while (sorted) {
-//         sorted = false;
-//         rows = filterTable.rows;
-//         for (i = 1; i < rows.length - 1; i++) {
-//             sortFlag = false;
-//             x = rows[i].getElementsByTagName("TD")[0];
-//             y = rows[i + 1].getElementsByTagName("TD")[0];
-//             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//                 sortFlag = true;
-//                 break;
-//             }
-//         }
-//         if (sortFlag) {
-//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//             sorted = true;
-//         }
-//     }
-// }
-
 //show Logic
+document.getElementById('data').innerHTML = temp;
 $(document).ready(function() {
-    $('#itemData.Lat').DataTable();
+    $('.td').DataTable({
+        "order": [
+            [12, "desc"]
+        ]
+    });
 });
+
+//total count of Confirmed cases
+
+var a = document.getElementById('ct');
+var rows = a.rows.length;
+alert("Number of Rows are: " + rows);
